f0f123450a22a6ad6a625a5570defa45865531da
Satabdi <satabdi.d@gmail.com>
2018-10-07 20:08:46 -0400

    Adding expanding minesweep
    Adding expanding minesweep

---
+++ Algorithms/src/org/tutorial/minesweepexpand/MS2.java
@@ -0,0 +1,43 @@
+package org.tutorial.minesweepexpand;
+
+public class MS2 {
+    public static void main(String[] args) {
+        // NOTE: The following input values will be used for testing your solution.
+        int[][] field1 = {{0, 0, 0, 0, 0},
+                          {0, 1, 1, 1, 0},
+                          {0, 1, -1, 1, 0}};
+
+        // click(field1, 3, 5, 2, 2) should return:
+        // [[0, 0, 0, 0, 0],
+        //  [0, 1, 1, 1, 0],
+        //  [0, 1, -1, 1, 0]]
+
+        // click(field1, 3, 5, 1, 4) should return:
+        // [[-2, -2, -2, -2, -2],
+        //  [-2, 1, 1, 1, -2],
+        //  [-2, 1, -1, 1, -2]]
+
+
+        int[][] field2 = {{-1, 1, 0, 0},
+                          {1, 1, 0, 0},
+                          {0, 0, 1, 1},
+                          {0, 0, 1, -1}};
+
+        // click(field2, 4, 4, 0, 1) should return:
+        // [[-1, 1, 0, 0],
+        //  [1, 1, 0, 0],
+        //  [0, 0, 1, 1],
+        //  [0, 0, 1, -1]]
+
+        // click(field2, 4, 4, 1, 3) should return:
+        // [[-1, 1, -2, -2],
+        //  [1, 1, -2, -2],
+        //  [-2, -2, 1, 1],
+        //  [-2, -2, 1, -1]]
+    }
+
+    // Implement your solution below.
+    public static int[][] click(int[][] field, int numRows, int numCols, int givenI, int givenJ) {
+        return field;
+    }
+ }
